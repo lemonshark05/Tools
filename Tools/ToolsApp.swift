70c436a1cbc4dcb347c0335c622d133d58162a4f
lemonshark <zmeng8@usfca.com>
2022-10-09 16:04:03 -0700

    Initial Commit

---
+++ Tools/ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolsApp.swift
+//  Tools
+//
+//  Created by lemonshark on 2022/10/9.
+//
+
+import SwiftUI
+
+@main
+struct ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
